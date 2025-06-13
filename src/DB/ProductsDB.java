5a90239dd8285784a5a9e085c6c140097463cd46
Jacobo Alan <jacobo.morles@gmail.com>
2025-06-13 17:29:45 -0600

    No creo que funcione

--- src/DB/ProductsDB.java
+++ src/DB/ProductsDB.java
@@ -1,7 +1,7 @@
 package DB;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javax.swing.plaf.nimbus.State;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,21 +20,62 @@ public class ProductsDB {
                     "ts_stat('SELECT to_tsvector(''simple'', description) FROM products');");
             st.executeUpdate("CREATE INDEX desc_wds_idx ON wds_description USING GIN(word gin_trgm_ops);");
 
-            
-
         } catch (SQLException e) {
             System.out.printf("Error. %s" , e.getMessage());
             System.exit(1);
         }
     }
 
-    static String parseQuary(String query) {
-        return query.replace(" ", " | ");
+    String getSimilar(String word) {
+        try {
+            PreparedStatement st =  this.db.prepareStatement("SELECT word FROM wds_description " +
+                    "WHERE similarity(word, ?) > 0.5;");
+
+            st.setString(1, word);
+
+            ResultSet rows = st.executeQuery();
+
+            word += " |";
+
+            while(rows.next()) {
+                word = word.concat(rows.getString("word") + " |") ;
+
+                // If it's the last rows don't add " |" at the end
+                if(!rows.next()) {
+                    rows.previous();
+                    word = word.concat(rows.getString("word"));
+                }
+            }
+
+            return word;
+        } catch (SQLException e) {
+            System.err.printf("Error: %s", e.getMessage());
+            return word;
+        }
     }
 
-    public int getProductID(String query) {
+    public int getProductsByDesc(String query) {
+        //
+        String parsedQuery = query.replaceAll("\\s+", " | ");
+
+        String allSimWords = "";
+        String[] words = query.split("\\s+");
+
+
+        for(int i = 0; i < words.length; i++) {
+            allSimWords = allSimWords.concat(getSimilar(words[i]) + " |");
+
+            if (i == words.length - 1 ) {
+                allSimWords = allSimWords.concat(getSimilar(words[i]));
+            }
+        }
+
         try {
-            Statement st = this.db.prepareStatement("SELECT *, wetsearch_to_tsquery('spansih', replace(?, ' ', ' OR '))");
+            PreparedStatement st = this.db.prepareStatement("SELECT * FROM products " +
+                                "WHERE to_tsvector(description) @@ to_tsquery(?);");
+
+            st.setString(1, allSimWords);
+
             return 1;
         } catch (SQLException e) {
             System.exit(1);
